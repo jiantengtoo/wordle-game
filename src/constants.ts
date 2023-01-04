72f07b1139eb3769d138216e9d417896d4cfeb18
JIAN TENG <aiyo321@gmail.com>
2023-01-04 19:46:05 +0800

    number of tries to 6

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,5 @@
 export const NUMBER_OF_LETTERS = 5;
-export const NUMBER_OF_TRIES = 2;
+export const NUMBER_OF_TRIES = 6;
 export const KEYBOARD_LAYOUT = [
   ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p"],
   ["a", "s", "d", "f", "g", "h", "j", "k", "l"],
